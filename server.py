b6920c38f4ddfe346e274b111afec82513d4724c
Fernanda <gilneidefernandaf@gmail.com>
2025-04-30 16:59:56 -0300

    adiciona comentários

--- server.py
+++ server.py
@@ -12,6 +12,10 @@ clientes = []
 class ServicoChat(rpc.ChatServiceServicer):
 
     def Chat(self, request_iterator, context):
+        """
+        Esta é uma chamada do tipo de fluxo de resposta. Isso significa que o servidor pode continuar enviando mensagens
+        Todo cliente abre essa conexão e espera o servidor enviar novas mensagens
+        """
         fila_cliente = []
         clientes.append(fila_cliente)
 
@@ -26,7 +30,8 @@ class ServicoChat(rpc.ChatServiceServicer):
                 clientes.remove(fila_cliente)
 
         threading.Thread(target=enviar_mensagens, daemon=True).start()
-
+        
+        # Para cada cliente, um loop infinito é iniciado (no próprio thread gerenciado do gRPC)
         while True:
             if fila_cliente:
                 yield fila_cliente.pop(0)
@@ -34,10 +39,15 @@ class ServicoChat(rpc.ChatServiceServicer):
 
 
 def iniciar_servidor():
+    # Workers é a quantidade de threads que podem ser abertos ao mesmo tempo.
+    # Se tiver 10 clientes conectados, então mais clientes não são capazes de se conectar ao servidor.
     servidor = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    rpc.add_ChatServiceServicer_to_server(ServicoChat(), servidor)
+    # Cria um servidor gRPC
+    rpc.add_ChatServiceServicer_to_server(
+        ServicoChat(), servidor
+    )  # registra a classe que implementa o serviço gRPC (no caso, ServidorChat) no servidor.
     servidor.add_insecure_port("[::]:50051")
-    servidor.start()
+    servidor.start()  # começa a escutar as requisições
     print("Servidor rodando em :50051")
     servidor.wait_for_termination()
 
